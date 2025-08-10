9d2df4b11e1c78f879af95149445926418798979
Hi <summer.ideac@gmail.com>
2025-08-11 01:20:56 +1000

    mock axios in jest; add testing deps on src>setupTests.js

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -2,4 +2,21 @@
 // allows you to do things like:
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
+
 import '@testing-library/jest-dom';
+
+// Mock axios for all tests to avoid importing ESM build from node_modules
+jest.mock('axios', () => {
+  const mockInstance = {
+    get: jest.fn(),
+    post: jest.fn(),
+    put: jest.fn(),
+    delete: jest.fn(),
+    interceptors: { request: { use: jest.fn() }, response: { use: jest.fn() } },
+  };
+  return {
+    __esModule: true,
+    default: { create: () => mockInstance },
+    create: () => mockInstance,
+  };
+});
