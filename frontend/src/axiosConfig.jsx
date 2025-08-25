a13e7abac3c4853cb4561e77b3a9ce02e35e8b99
Hi <summer.ideac@gmail.com>
2025-08-25 16:22:16 +1000

    updated yml

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  //baseURL: 'http://localhost:5001', // local
-  baseURL: '13.236.85.224:5001', // live
+  baseURL: 'http://localhost:5001', // local
+  //baseURL: '13.236.85.224:5001', // live
   headers: { 'Content-Type': 'application/json' },
 });
 
